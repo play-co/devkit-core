635e4f952e3c717f6878aa338a35bf7f4be7be13
Tom Fairfield <fairfieldt@gameclosure.com>
2014-09-10 13:56:18 -0700

    mocks out some more DOM apis on native

--- src/clientapi/native/Document.js
+++ src/clientapi/native/Document.js
@@ -13,23 +13,62 @@
  * You should have received a copy of the Mozilla Public License v. 2.0
  * along with the Game Closure SDK.  If not, see <http://mozilla.org/MPL/2.0/>.
  */
-
+import device;
 if (typeof document == 'undefined') {
 	GLOBAL.document = {};
 }
 
+var createDummyObject = function(parent, name) {
+    var dummyObj = {};
+    parent.__defineGetter__(name, function() {
+        logger.warn('Accessing', name, 'is a dummy object!');
+        return dummyObj;
+    });
+};
+
+var addDOMAPI = function(el) {
+    el.getBoundingClientRect = function() {
+        return {
+            left: 0,
+            top: 0,
+            right: this.width,
+            bottom: this.height,
+            width: this.width,
+            height: this.height
+        };
+    };
+};
+
+if (!document.body) {
+    createDummyObject(document, 'body');
+}
+if (!document.documentElement) {
+    createDummyObject(document, 'documentElement');
+}
+document.body.appendChild = function() {
+    logger.warn('document.body.appendChild is unimplemented');
+};
+
+document.getElementById = function() {
+    logger.warn('document.getElementById is not implemented');
+    return undefined;
+};
+
 if (!document.createElement) {
 	var handlers = {};
-	
 	document.createElement = function (type) {
-		type = type.toUpperCase(); 
+		type = type.toUpperCase();
 		if (type in handlers) {
 			return handlers[type]();
 		}
-	}
-	
+	};
+
 	document.__registerCreateElementHandler = function (type, cb) {
 		type = type.toUpperCase();
 		handlers[type] = cb;
-	}
+	};
 }
+addEventListenerAPI(window);
+addEventListenerAPI(document);
+
+document.readyState = 'complete';
