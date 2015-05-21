61ea7e05978aedfc42b8e0a2e27ae5b85a449076
Martin Hunt <martin@gameclosure.com>
2015-05-20 22:58:20 -0700

    wrap vinyl files so we can use streams without creating the stream until first use (avoid opening too many files)

--- src/build/common/resources/index.js
+++ src/build/common/resources/index.js
@@ -1,7 +1,38 @@
 var path = require('path');
 var fs = require('fs');
+var util = require('util');
+
 var File = require('vinyl');
 var glob = Promise.promisify(require('glob'));
+
+// vinyl-fs reads all files as buffers or streams. Many files we don't actually
+// end up copying, so we want to be somewhat lazy about them. The StreamFile
+// class generates the content stream only on first use, which also helps
+// avoid emfile errors (too many open files).
+function StreamingFile(opts) {
+  File.apply(this, arguments);
+
+  this._originalPath = opts.path;
+}
+
+util.inherits(StreamingFile, File);
+
+StreamingFile.prototype.isStream = function () { return true; };
+
+Object.defineProperty(StreamingFile.prototype, 'contents', {
+  get: function () {
+    if (!this._contents) {
+      this._contents = fs.createReadStream(this._originalPath);
+    }
+
+    return this._contents;
+  },
+  set: function (stream) {
+    // vinyl-fs recreates the stream after copying it, which means we might run
+    // out of file descriptors. Don't save the new streams in hopes that they
+    // get gc'd relatively quickly
+  }
+});
 
 exports.getDirectories = require('./directories').get;
 exports.getMetadata = require('./metadata').get;
@@ -13,10 +44,10 @@ exports.getFiles = function (targetDirectory, directories) {
         // the history is the location on disk, then we set the path to the
         // target location
         var srcPath = path.join(directory.src, filename);
-        var file = new File({
+
+        var file = new StreamingFile({
           base: directory.src,
-          path: srcPath,
-          contents: fs.createReadStream(srcPath)
+          path: srcPath
         });
 
         file.originalRelativePath = filename;
